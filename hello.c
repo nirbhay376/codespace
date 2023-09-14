caa44f76befc3f740ff112d56420864fd4c6c465
nirbhay376 <130355179+nirbhay376@users.noreply.github.com>
2023-09-14 11:21:06 +0000

    Thu, Sep 14, 2023, 11:21 AM +00:00

--- hello.c
+++ hello.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include <cs50.h>
 
 int main(void)
 {
-    string name = get_string("what's your name? ");
-    printf("hello, %s\n", name);
+
+    printf("hello world"\n");
 }
 
 
